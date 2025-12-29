dfca8dc33d2a8382d1e4558fb52d11d4714852fd
Robbie McKinstry <robbie@wack.run>
2025-12-29 17:09:19 -0500

    Fix Type errors in Controller Skeleton
    This commit fixes type errors in the controller skeleton.
    I was struggling to allow the controller to dispatch the right
    type to the run loop.

--- crates/gateway-crds/src/lib.rs
+++ crates/gateway-crds/src/lib.rs
@@ -1,16 +1,16 @@
+pub use backend_tls_policies::*;
+pub use gateway::*;
 pub use gateway_class::*;
+pub use grpc::*;
 pub use http::*;
 pub use reference_grants::*;
-pub use gateway::*;
-pub use grpc::*;
-pub use backend_tls_policies::*;
 
+mod backend_tls_policies;
+mod gateway;
 mod gateway_class;
+mod grpc;
 mod http;
 mod reference_grants;
-mod gateway;
-mod grpc;
-mod backend_tls_policies;
 
 #[cfg(test)]
 mod tests {
